33b2bb1b97684f600a765fa5a717b1991c8727c1
bkings <bbikings@yahoo.com>
2020-12-06 00:55:38 +0545

    port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('./startup/db')();
 require('./startup/routes')(app);
 require('./startup/validation')();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () => console.log(`Listening on port ${port} ...`));
 
 module.exports = server;
